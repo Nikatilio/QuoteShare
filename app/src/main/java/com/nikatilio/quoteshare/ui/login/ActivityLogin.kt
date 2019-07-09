e17b6bb9a096589cb29f74d267af4e67d9058ba4
Nikita Kazantsev <enzomatrix89@gmail.com>
2019-07-08 22:25:14 -0500

    Added account login code

--- app/src/main/java/com/nikatilio/quoteshare/ui/login/ActivityLogin.kt
+++ app/src/main/java/com/nikatilio/quoteshare/ui/login/ActivityLogin.kt
@@ -2,9 +2,13 @@ package com.nikatilio.quoteshare.ui.login
 
 import android.accounts.AccountAuthenticatorActivity
 import android.accounts.AccountManager
+import android.accounts.AccountManagerCallback
+import android.accounts.AccountManagerFuture
 import android.app.Activity
 import android.content.Intent
 import android.os.Bundle
+import android.os.Handler
+import android.os.Message
 import android.widget.Toast
 import com.nikatilio.quoteshare.R
 import kotlinx.android.synthetic.main.activity_login.*
@@ -36,7 +40,30 @@ class ActivityLogin: AccountAuthenticatorActivity() {
         val username = loginNameEditText.text
         val password  = loginPassEditText.text
 
-        val accountType = intent.getStringExtra(AccountManager.KEY_ACCOUNT_TYPE)
+        var accountType = intent.getStringExtra(AccountManager.KEY_ACCOUNT_TYPE)
+        if (accountType.isNullOrEmpty()) {
+            accountType = getString(R.string.account_type)
+        }
+
+        val allAccounts = accountManager.accounts
+
+        val accounts = accountManager.getAccountsByType(accountType)
+        if (accounts.isNullOrEmpty()) { // Account not found. Create a new account
+
+
+
+        } else {
+            // Do I need to check if there's more then one account with give accountType?
+            val options = Bundle()
+            accountManager.getAuthToken(
+                accounts[0],
+                accountType,
+                options,
+                this,
+                onTokenAcquired(),
+                Handler(OnError())
+                )
+        }
 
         // Async operation
         println(accountType)
@@ -50,6 +77,20 @@ class ActivityLogin: AccountAuthenticatorActivity() {
             println("SUCCESSFUL REGISTRATION")
         } else {
             super.onActivityResult(requestCode, resultCode, data)
+        }
+    }
+
+    class onTokenAcquired: AccountManagerCallback<Bundle> {
+        override fun run(future: AccountManagerFuture<Bundle>?) {
+            val bundle = future?.result
+            val token = bundle?.getString(AccountManager.KEY_AUTHTOKEN)
+            println("Token: $token")
+        }
+    }
+
+    class OnError: Handler.Callback {
+        override fun handleMessage(msg: Message?): Boolean {
+            TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
         }
     }
 }
