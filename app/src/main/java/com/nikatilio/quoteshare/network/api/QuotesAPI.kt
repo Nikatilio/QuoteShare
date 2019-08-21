6da667e4530c191ceb7dc43f465dfc81eb8369b8
Nikita Kazantsev <enzomatrix89@gmail.com>
2019-08-20 18:46:44 -0700

    Added api interface and data model.

---
+++ app/src/main/java/com/nikatilio/quoteshare/network/api/QuotesAPI.kt
@@ -0,0 +1,26 @@
+package com.nikatilio.quoteshare.network.api
+
+import com.nikatilio.quoteshare.data.QuoteInfo
+import com.nikatilio.quoteshare.data.QuotesList
+import kotlinx.coroutines.Deferred
+import retrofit2.Response
+import retrofit2.http.Body
+import retrofit2.http.GET
+import retrofit2.http.POST
+import retrofit2.http.Path
+
+interface QuotesAPI {
+
+    @POST("/api/session")
+    fun createSession(@Body credentials: UserCredentials): Deferred<Response<Session>>
+
+    @GET("/api/qotd")
+    fun quoteOfTheDay(): Deferred<Response<QuoteInfo>>
+
+    @GET("/api/quotes")
+    fun quotesList(): Deferred<Response<QuotesList>>
+
+    @GET("/api/quotes/{id}")
+    fun getQuote(@Path("id") id:Int): Deferred<Response<QuoteInfo>>
+
+}
---
+++ app/src/main/java/com/nikatilio/quoteshare/network/api/UserCredentials.kt
@@ -0,0 +1,5 @@
+package com.nikatilio.quoteshare.network.api
+
+data class UserCredentials(
+    val user: User
+)
